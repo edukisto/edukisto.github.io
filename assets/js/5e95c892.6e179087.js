4e00c1b023527e2a91653021ad66a4e96ac30cc2
edukisto <52005215+edukisto@users.noreply.github.com>
2023-12-02 08:48:15 +0300

    Add

---
+++ assets/js/5e95c892.6e179087.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_edukisto_tlm=self.webpackChunk_edukisto_tlm||[]).push([[661],{4942:function(e,s,t){t.r(s),t.d(s,{default:function(){return l}});t(7294);var u=t(6010),r=t(5463),n=t(3702),c=t(8790),a=t(8718),i=t(5893);function l(e){return(0,i.jsx)(r.FG,{className:(0,u.Z)(n.k.wrapper.docsPages),children:(0,i.jsx)(a.Z,{children:(0,c.H)(e.route.routes)})})}}}]);
