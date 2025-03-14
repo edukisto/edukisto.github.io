fdbe53845c22270711d17635becc51b1c42f385c
edukisto <52005215+edukisto@users.noreply.github.com>
2025-03-14 11:05:35 +0300

    Fix

--- assets/js/5e95c892.5cb78ab6.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_edukisto_tlm=self.webpackChunk_edukisto_tlm||[]).push([[9647],{3124:function(e,s,t){t.r(s),t.d(s,{default:function(){return l}});t(6540);var u=t(4164),r=t(9817),n=t(8630),c=t(2831),a=t(6422),i=t(4848);function l(e){return(0,i.jsx)(r.e3,{className:(0,u.A)(n.G.wrapper.docsPages),children:(0,i.jsx)(a.A,{children:(0,c.v)(e.route.routes)})})}}}]);
